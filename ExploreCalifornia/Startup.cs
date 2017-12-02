0a2faa6a5b785ca037ddbce3b27b7d9fc73e1955
Edwin Marte Torres <kdgpro15@gmail.com>
2017-12-01 21:05:07 -0400

    Chapter 1 - The Basics (#1)
    * Installation of Microsoft.AspNetCore.StaticFiles through the NuGet Package Manager
    * Adding and serving the Static Files to the wwwroot folder of the application.
    * Implementing and Understanding Error Handlers
    * Working on custom Configuration.
    * Adding the environment variables dictionary with the configuration setting `new ConfigurationBuilder().AddEnvironmentVariables().Build();`.
    * Adding Json File to the configuration variables
    * Creating a FeatureToggles Class to decouple the environments
    * Defining the Injection Options in the ConfigureServices method
    * Created Startup Method which creates the new custom configurations instance for the app

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -6,29 +6,61 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace ExploreCalifornia
 {
     public class Startup
     {
+
+        private readonly IConfigurationRoot configuration;  //Variable for the configurations
+
+        // This method gets called by the runtime. It's used to call the Environment variables and configurations on startup of application
+        public Startup(IHostingEnvironment env)
+        {
+            // Creating custom configurations
+            configuration = new ConfigurationBuilder()
+                                  .AddEnvironmentVariables()
+                                  .AddJsonFile(env.ContentRootPath + "/config.json")
+                                  .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
+                                  .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // A transient to be used as a instance configuration every time it's requested for the FeaturesToggle Class
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, FeatureToggles features)
         {
-            if (env.IsDevelopment())
+            loggerFactory.AddConsole();
+
+            app.UseExceptionHandler("/error.html");
+
+            // Evaluating with the custom configurations files and environment variables
+            if (features.EnableDeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.Use(async (context, next) => 
             {
-                await context.Response.WriteAsync("Hello World!");
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+
+                await next();
             });
+
+            // Using the `Microsoft.AspNetCore.StaticFiles` to serve static files
+            app.UseFileServer();
         }
     }
 }
