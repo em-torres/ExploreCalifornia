2ded2265ed2c52fad352625299626b2bd95e3b77
Edwin Marte Torres <kdgpro15@gmail.com>
2017-12-01 20:57:29 -0400

    Finishing Chapter 1 and wrapping up

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -7,25 +7,46 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace ExploreCalifornia
 {
     public class Startup
     {
+
+        private readonly IConfigurationRoot configuration;  //Variable for the configurations
+
+        // This method gets called by the runtime. It's used to call the Environment variables and configurations on startup of application
+        public Startup(IHostingEnvironment env)
+        {
+            // Creating custom configurations
+            configuration = new ConfigurationBuilder()
+                                  .AddEnvironmentVariables()
+                                  .AddJsonFile(env.ContentRootPath + "/config.json")
+                                  .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
+                                  .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // A transient to be used as a instance configuration every time it's requested for the FeaturesToggle Class
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, FeatureToggles features)
         {
             loggerFactory.AddConsole();
 
             app.UseExceptionHandler("/error.html");
 
-            if (env.IsDevelopment())
+            // Evaluating with the custom configurations files and environment variables
+            if (features.EnableDeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -38,6 +59,7 @@ namespace ExploreCalifornia
                 await next();
             });
 
+            // Using the `Microsoft.AspNetCore.StaticFiles` to serve static files
             app.UseFileServer();
         }
     }
--- TrainingNotes.md
+++ TrainingNotes.md
@@ -1,7 +1,7 @@
 # Project Notes
 ---------------------
 
-## Chapter 1:
+## Chapter 1: The Basics
 #### Module 4:
 * Installation of `Microsoft.AspNetCore.StaticFiles` through the **NuGet Package Manager** to manage Static Files on the project.
 * I can use `Ctrl + .` to install from the context menu when the IDE recognizes the package I'm trying to install.
@@ -13,3 +13,30 @@
 #### Module 6:
 * Implementing and Understanding Error Handlers for Development and Production Envs.
 * Using the `app.UseExceptionHandler()` to set a Production Custom Error Page.
+
+#### Module 7:
+* Working on custom Configuration.
+* Installation of Configuration Package and creating the instance.
+* Adding the environment variables dictionary with the configuration setting `new ConfigurationBuilder().AddEnvironmentVariables().Build();`.
+* Adding Environment Variables through the App Properties.
+
+#### Module 8:
+* Adding Json File to the configuration variables with `.AddJsonFile(file_location)`. For Example:
+```
+var configuration = new ConfigurationBuilder()
+							.AddEnvironmentVariables()
+							.AddJsonFile(env.ContentRootPath + "/config.json")
+							.Build();
+```
+* Learning how to use the Debugger
+* The second parameter in the `.AddJsonFile()` method allows the Configuration instance to look for a file and if doesn't exists it continues running the program.
+
+#### Module 9:
+* Creating a `FeatureToggles` Class to decouple the environments from the configuration setting to prevent injection.
+* Defining the Injection Options in the `ConfigureServices` method adding a **Transient** (creating an instance every time is requested).
+* In ASP.NET MVC we use the **Scoped** (a single instance for each scope).
+* The **Singleton** creates a single instance for the entire application.
+* We created a **Startup** Method which creates the new custom configurations instance for the app on startup for dependency injection.
+
+---------------------
+## Chapter 2: The MVC Pattern
